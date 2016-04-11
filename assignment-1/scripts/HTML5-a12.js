fbdabfad8aa663c7b6daaeab8d9dac18f39f5427
Sean C <sean@scavanaugh.com>
2016-04-11 14:49:10 -0500

    More troubleshooting

--- assignment-1/scripts/HTML5-a12.js
+++ assignment-1/scripts/HTML5-a12.js
@@ -1,45 +1,15 @@
 //$(document).ready(
 
-var submitButton = $("#submitEntry");
-
-submitButton.on("click", addInput);
 
 var userEntries = [];
-var userSum = [];
-var userAverage = [];
+//var userSum = [];
+//var userAverage = [];
 
 var userEntry = $("#userInput").val();
 //userEntry = parseInt(userEntry);
 //var notNum = NaN;
 
-function addInput(evt){
-	
-	userEntries.push(userEntry);
-	//var sum = sumCalc;
-	
-		//console.log(userEntry);
-
-
-		/*if (userEntry === NaN){
-			alert("That is not a number");
-		}*/
-
-		/*else{
-			console.log(userEntry);
-		}*/
-		//else{
-			
-			//var sumOutput = userEntries[0] + userEntry[1];
-			//average = sum / userEntries.length
-
-		//} 			
-};
-
-$("#entries").html(userEntries.length);//sends the number of indexes in the array to the html li
-
-
-
-total = 0;
+var total 
 var sum = function sumCalc(){
 	for(var i = 0; i < userEntries.length; i++){
 		total = (total + userEntries[i]);
@@ -49,8 +19,6 @@ var sum = function sumCalc(){
 $("#sum").html(sum);//sends the sum of all values in the array to the html li
 
 
-
-
 var average = function avCalc(){
 	var av = (sum / userEntries.length);
 	return av;
@@ -58,15 +26,35 @@ var average = function avCalc(){
 $("#average").html(average);//sends the average of value in the array to the html li
 
 
+function addInput(evt){
+	
+	userEntries.push(userEntry);
+	//var sum = sumCalc;
+	//console.log(userEntry);
+		if (userEntry === NaN){
+			alert("That is not a number");
+		}
+
+		else{
+			sum;
+			average;
+			//console.log(userEntry);
+		}			
+};
+
+$("#entries").html(userEntries.length);//sends the number of indexes in the array to the html li
 
 
+var submitButton = $("#submitEntry");
+submitButton.on("click", addInput);
 
-/*var resetButton = $("#reset");
+
+var resetButton = $("#reset");
 resetButton.on("click", resetForm);
 
 function resetForm(){
 	location.reload();
-};*/
+};
 
 //)end document ready
 
