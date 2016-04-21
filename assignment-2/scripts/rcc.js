badbf12e57ca702338630bd32a7501f2ff9154c0
sean c <sean@scavanaugh.com>
2016-04-21 11:53:30 -0500

    A commit

--- assignment-2/scripts/rcc.js
+++ assignment-2/scripts/rcc.js
@@ -1,26 +1,43 @@
-//$(document).ready(function() {
+$(document).ready(init);
 
-var recordCollection = [];
+//var recordCollection = [];
 //recordCollection[0] = recordProps[0], recordProps[1], recordProps[2];
 
-function commitNewRecord(){
-
+function init(){
+  
+  
+  var addButton = $('#addnew').on("click", showAddDisplay);
+  var commitButton = $('#commitnew').on("click", commitNewRecord);
+  
+  function showAddDisplay(evt){
+    $("#adddisplay").removeClass("hide");
+    $("#adddisplay").addClass("show");
+    //change css to show div
+  };
+  
+  
+  function commitNewRecord(evt){
+  evt.preventDefault();
   var addProp0 = $('#artist').text();
   var addProp1 = $('#title').text();
   var addProp2 = $('#label').text();
-  
-  var addRecord = $('#catalogue').append('<tr><td>addProp0</td><td>addProp1</td><td>addProp2</td></tr>');//gets tbody and adds tr for the item and tds for it's properties
+  var addRecord = $('#catalogue').append('<tr><td>' + addProp0 + 'test</td><td>' + addProp1 + '</td><td>' + addProp2 + '</td></tr>');
   var recordProps = [];
   recordProps.push(addProp0);
   recordProps.push(addProp1);
   recordProps.push(addProp2);
+  var recordCollection = [];
+  recordCollection.push(recordProps);
+  $("#adddisplay").removeClass("show");
+  $("#adddisplay").addClass("hide");  
+  };
  
   
 //recordProps[0] = "";
 //recordProps[1] = "";
 //recordProps[2] = "";
-  recordCollection.push(recordProps);
-};//end newRecord
-//};//end ready
 
-commitNewRecord();
+  
+};//end init
+
+//commitNewRecord();
