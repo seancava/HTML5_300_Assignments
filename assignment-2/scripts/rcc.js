7be23ed9678e940b0a9f5e7ddbe88af2bc14eba6
sean c <sean@scavanaugh.com>
2016-04-21 12:09:21 -0500

    It's working

--- assignment-2/scripts/rcc.js
+++ assignment-2/scripts/rcc.js
@@ -6,30 +6,31 @@ $(document).ready(init);
 function init(){
   
   
-  var addButton = $('#addnew').on("click", showAddDisplay);
-  var commitButton = $('#commitnew').on("click", commitNewRecord);
+  var addButton = $('#addnew').on('click', showAddDisplay);
+  var commitButton = $('#commitnew').on('click', commitNewRecord);
   
   function showAddDisplay(evt){
-    $("#adddisplay").removeClass("hide");
-    $("#adddisplay").addClass("show");
+    $('#adddisplay').removeClass('hide');
+    $('#adddisplay').addClass('show');
     //change css to show div
   };
   
   
   function commitNewRecord(evt){
   evt.preventDefault();
-  var addProp0 = $('#artist').text();
-  var addProp1 = $('#title').text();
-  var addProp2 = $('#label').text();
-  var addRecord = $('#catalogue').append('<tr><td>' + addProp0 + 'test</td><td>' + addProp1 + '</td><td>' + addProp2 + '</td></tr>');
+  var addProp0 = $('#artist').val();
+  var addProp1 = $('#title').val();
+  var addProp2 = $('#label').val();
+  var addRecord = $('#catalogue').append('<tr><td>' + addProp0 + '</td><td>' + addProp1 + '</td><td>' + addProp2 + '</td></tr>');
   var recordProps = [];
   recordProps.push(addProp0);
   recordProps.push(addProp1);
   recordProps.push(addProp2);
   var recordCollection = [];
   recordCollection.push(recordProps);
-  $("#adddisplay").removeClass("show");
-  $("#adddisplay").addClass("hide");  
+  $('#adddisplay').removeClass('show');
+  $('#adddisplay').addClass('hide'); 
+  $('form').trigger('reset');  
   };
  
   
