1a9dfa057bb570c4ea1a4658925bd82542eb6e35
sean c <sean@scavanaugh.com>
2016-04-26 23:33:30 -0400

    another commit

--- assignment-3/HTML5-a32.js
+++ assignment-3/HTML5-a32.js
@@ -1,89 +1,47 @@
 $(document).ready(init);
 function init(){
-  
+
 var submitButton = $("#submitEntry");
 submitButton.on("click", addInput);
 
 var userNumberEntries = [];
 var userStringEntries = [];
-//$("#entries").html(userEntries.length);//sends the number of indexes in the array to the html 
 
-  
 function addInput(evt){
-  evt.preventDefault();
-  //$("form").reset();
-	var userEntry = $("#userInput").val();
-  if (isNaN(userEntry)){
-    userStringEntries.push(userEntry);
-  }
-  else{
-    parseInt(userEntry);
-    userNumberEntries.push(userEntry);
-  }
-  
-  //var entriesNumberCount = userNumberEntries.length;
-  //var stringCon = userStringEntries.join(' ');
-  
-  console.log(userNumberEntries);
-  console.log(userStringEntries);
-  //console.log(userEntry);
-  //console.log(userEntries);
-  //console.log(userEntries.join(' '));
-  //console.log(entries);
+evt.preventDefault();
+//$("form").reset();
+var userEntry = $("#userInput").val();
+parseInt(userEntry, 10);
+if (isNaN(userEntry)){
+userStringEntries.push(userEntry);
+}
+else{
 
+userNumberEntries.push(userEntry);
+}
 
-  //var total = userEntries[0]; 
- // var sum = function(){
- //   for(var i = 0; i < userEntries.length; i++){
- //     total = (total + userEntries[i]);
- //   }
- //   return userEntries.join(' ');
- // };
-//
+var sum = 0; 
+for (var i = 0; i < userNumberEntries.length; i++) {
+sum += userNumberEntries[i] << 0;
+}
 
 $("#totalNumbers").html
-("The total number of numbers entered is " + userNumberEntries.length);
+("The total number of numbers entered is: " + userNumberEntries.length);
 $("#sumNumbers").html
-("The sum of numbers entered is ");
+("The sum of numbers entered is: " + sum);
 $("#averageNumbers").html
-("The average of numbers entered is ");
+("The average of numbers entered is: " + sum / (userStringEntries.length));
 $("#totalStrings").html
-("The total number of strings entered is " + userStringEntries.length);
+("The total number of strings entered is: " + userStringEntries.length);
 $("#concatStrings").html("The strings concatenated is: " + userStringEntries.join(' '));
-//$("#sum").html(string);//sends the sum of all values in the array to the html li
-//
-//
-//	var average = function avCalc(){
-//	var av = (sum / userEntries.length);
-//	return av;
-//	};
-//$("#average").html(average);//sends the average of value in the array to the html li
-//
-//	userEntries.push(userEntry);
-//	//var sum = sumCalc;
-//	//console.log(userEntry);
-//		if (userEntry === NaN){
-//			alert("That is not a number");
-//		}
-//
-//		else{
-//			sum;
-//			average;
-//			//console.log(userEntry);
-//		}			
+
 };//end addInput
-
-
-
-
-
-
 
 var resetButton = $("#reset");
 resetButton.on("click", resetForm);
 
 function resetForm(){
-	location.reload();
+location.reload();
 };
 }//end init
 
@@ -101,4 +59,3 @@ function resetForm(){
 
 
 
-	
