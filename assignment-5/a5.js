264fccb60f0b35f1b7b3e59a20c319cab84d1aa8
sean c <sean@scavanaugh.com>
2016-05-10 23:37:23 -0400

    More dev

--- assignment-5/a5.js
+++ assignment-5/a5.js
@@ -1,11 +1,9 @@
 $(document).ready(init);
 function init(){
   $('#submit').on('click', update);
-  
-  
-  
+    
   function update(evt){
-    //evt.preventDefault();
+      evt.preventDefault();
       var checkboxValue = $('#checkbox').is(':checked');
       var radValue = $('input[name="rad"]:checked').val();
       var textValue = $('#textInput').val();
@@ -14,7 +12,7 @@ function init(){
       var colorValue = $('#color').val();
       var dateValue = $('#date').val();
       var dateTimeValue = $('#datetime').val();
-      var dateTimeLocal = $('#datetime-local').val();
+      var dateTimeLocalValue = $('#datetime-local').val();
       var monthValue = $('#month').val();
       var emailValue = $('#email').val();
       var numberValue = $('#number').val();
@@ -22,8 +20,7 @@ function init(){
       var rangeValue = $('#range').val();
       var searchValue = $('#search').val();
       var telValue = $('#tel').val();
-//    
-//    
+   
     $('#checkVal').append('<td id="checkVal">' + checkboxValue + '</td');
     $('#radVal').append('<td id="radVal">' + radValue + '</td');
     $('#textVal').append('<td id="textVal">' + textValue + '</td');
@@ -32,26 +29,17 @@ function init(){
     $('#colorVal').append('<td id="colorVal">' + colorValue + '</td');
     $('#dateVal').append('<td id="dateVal">' + dateValue + '</td');
     $('#datetimeVal').append('<td id="dateTimeVal">' + dateTimeValue + '</td');  
-    $('#dateTimeLocalVal').append('<td id="dateTimeLocalVal">' + dateTimeValue + '</td');
+    $('#dateTimeLocalVal').append('<td id="dateTimeLocalVal">' + dateTimeLocalValue + '</td');
     $('#monthVal').append('<td id="monthVal">' + monthValue + '</td');
     $('#emailVal').append('<td id="emailVal">' + emailValue + '</td');
     $('#numberVal').append('<td id="numberVal">' + numberValue + '</td');
     $('#passwordVal').append('<td id="passwordVal">' + passwordValue + '</td');
     $('#rangeVal').append('<td id="rangeVal">' + rangeValue + '</td');
     $('#searchVal').append('<td id="searchVal">' + searchValue + '</td');
-     $('#telVal').append('<td id="telVal">' + telValue + '</td');
+    $('#telVal').append('<td id="telVal">' + telValue + '</td');
+    
+    $('#form').trigger("reset");
 
   };//end update
-  
-  
-  
-  
-  
-  
-
-  
-  
-  
-  
-  
+    
 }//end init
