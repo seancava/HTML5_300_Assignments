d391dd3dc80354f09cf54028ee19d3d7c11f98fc
sean c <sean@scavanaugh.com>
2016-05-17 14:37:12 -0400

    More dev

--- assignment-6/scripts/rcc.js
+++ assignment-6/scripts/rcc.js
@@ -1,63 +1,97 @@
 $(document).ready(init);
 
 function init(){
-  
- var addButton = $('#addnew').on('click', showAddDisplay);
+var baseUrl = 'https://shielded-sea-3725.herokuapp.com/data-api/';
+var collection = 'scavanaugh';
+var addButton = $('#addnew').on('click', showAddDisplay); 
+
   function showAddDisplay(evt){
     $('#adddisplay').removeClass('hide');
     $('#adddisplay').addClass('show');
-    //change css to show div
   };
   
-  var commitButton = $('#commitnew').on('click', commitNewRecord);//POST
+  $('#commitnew').off();
+  $('#commitnew').on('click', commitNewRecord);//POST//crudCREATE
+  
   function commitNewRecord(evt){
   evt.preventDefault();
-  var addProp0 = $('#artist').val();
-  var addProp1 = $('#title').val();
-  var addProp2 = $('#label').val();
-  var addRecord = $('#catalogue').append('<tr><form><td class="addprop">' + addProp0 + '</td><td class="addprop">' + addProp1 + '</td><td class="addprop">' + addProp2 + '</td><td><button id="delete">Delete</button></td><td><button id="update">Update</button></td></form></tr>');
-  var recordProps = [];
-  recordProps.push(addProp0);
-  recordProps.push(addProp1);
-  recordProps.push(addProp2);
-  var recordCollection = [];
-  recordCollection.push(recordProps);
-  $('#adddisplay').removeClass('show');
-  $('#adddisplay').addClass('hide'); 
-  $('form').trigger('reset');  
-  };
+
+    var addArtist = $('#artist').val();
+    var addTitle = $('#title').val();
+    var addLabel = $('#label').val();
+    var addDate = new Date();
+    
+    $.ajax( baseUrl + collection,
+    {
+        method: 'POST',
+        data: {
+                  artist: addArtist,
+                  title: addTitle,
+                  label: addLabel,
+                  date: addDate,
+              },
+        success: function logListResult( data ) {
+            console.log( 'Data received:', data );
+        },
+        error: logAjaxError
+    } );
+
+function logAjaxError( jqXHR, textStatus, errorThrown ) {
+    console.log( 'AJAX error. Status:', textStatus, 'error:', errorThrown );
+  }
+};//commitNewRecord();
   
-  var deleteButton = $('#delete').on('click', deleteRecord);//DELETE
+
+  
+  var deleteButton = $('#delete').on('click', deleteRecord);//DELETE//crudDELETE
   function deleteRecord(){
     
   };
   
-  var updateButton = $('#update').on('click', updateRecord);//Put
-  function updateRecord(){
+  var updateButton = $('#update').on('click', updateRecord);
+  function updateRecord(){//PUT//crudUPDATE
     
   };
   
-  var searchButton = $('#search').on('click', searchRecord);//GET
-  var searchRecord = $.ajax('myreqestRL.com/', {
+  var searchInput = $('#searchInput').val();
+  var searchButton = $('#search').on('click', searchRecord);
+  function searchRecord(evt){
+    $.ajax(baseUrl + collection + '/' + searchInput, {//GET//crudREAD
     method: 'GET',
-    data: {
-      example: 1,
-      eg: 2,
-      },
-    success: function logListResult(data){
-      console.log('Data received: ', data);
-  },
+    success: logListResult,
     error: logAjaxError
   });
+  
+    function logListResult(data){
+      console.log('Data received: ', data);
+  };
+    
+  
   function logAjaxError(jqXHR, textStatus, errorThrown){
-    console.log('AJAX error Status: ', textStatus, 'error', errorThrown);
-}
+    console.log('AJAX error Status: ', textStatus, 'error: ', errorThrown);
+  };
+};
   
-//recordProps[0] = "";
-//recordProps[1] = "";
-//recordProps[2] = "";
 
-  
 };//end init
 
-//commitNewRecord();
+
+
+
+
+
+
+
+
+  
+//  var addRecord = $('#catalogue').append('<tr><form><td class="addprop">' + addArtist + '</td><td class="addprop">' + addTitle + '</td><td class="addprop">' + addLabel + '</td><td class="addprop">' + addDate + '</td><td><button id="delete">Delete</button></td><td><button id="update">Update</button></td></form></tr>');
+//  var recordProps = [];
+//  recordProps.push(addArtist);
+//  recordProps.push(addTitle);
+//  recordProps.push(addLabel);
+//  var recordCollection = [];
+//  recordCollection.push(recordProps);
+//  $('#adddisplay').removeClass('show');
+//  $('#adddisplay').addClass('hide'); 
+//  $('form').trigger('reset');  
+//  };
