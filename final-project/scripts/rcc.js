01934521c854fbaf2b6e1ecb149e03389ae1d784
seanc <sean@scavanaugh.com>
2016-06-03 01:16:37 -0400

    more experimenting, trying to figure out how to call a unique property name from local storage

--- final-project/scripts/rcc.js
+++ final-project/scripts/rcc.js
@@ -1,74 +1,95 @@
 $(document).ready(init);
 
 function init(){
-//  localStorage.aKey = "aValue";//ad new data or change existing
-//  var value = localStorage.aKey//you can read a value like this//aKey=_id
-//  localStorage.removeItem ( "aKey" );//remove an item
-//  localStorage.clear();//remove all data
-//  localStorage.length// the number of key value pairs
-//  localocalStorageorage.key(n)//access the value of the key
+  
+  $('#listcount').append('<li id="listcount">' + recordCollectionLength + '</span>');
+  var start = new Date().getFullYear();
+  var end = 1910;
+  for(var year = start ; year >=end; year--){
+      $('#yearInput').append('<option>' + year + '</option>');
+    };//sets dates in year dropdown
   
   
-  var userName;
-  
-  var collectionName;
-  
-  var deleteButton = $('#delete');
+  function deleteRecord(evt){
+    localStorage.removeItem ( "record" );
+  };//end delete
+  var deleteButton = $('delete');
   deleteButton.off();
   deleteButton.on('click', deleteRecord);
   
+  
+  function updateRecord(evt){//PUT//crudUPDATE
+//    evt.preventDefault();
+    var addArtist = $('#artist').val();
+    var addTitle = $('#title').val();
+    var addLabel = $('#label').val();
+    var addDate = new Date();
+  };//end update
   var updateButton = $('#update');
   updateButton.off();
   updateButton.on('click', updateRecord);
-  
-  var commitButton = $('#commitnew');
-  commitButton.off();
-  commitButton.on('click', commitNewRecord);
-  
-  var searchInput = $('#searchInput').val();
-  
-  var searchButton = $('#search').on('click', searchRecord);
-  
-  var clearButton = $('#clearlocal').on('click', clearLocal);
-  
-  var addButton = $('#addnew').on('click', showAddDisplay);
-  
-  var hideDisplayButton = $('#hidepanel').on('click', hideAddDisplay);
-  
-  var autoSync = ('#autosync');
-  
-  var start = new Date().getFullYear();
-  var end = 1910;
-    for(var year = start ; year >=end; year--){
-        $('#yearInput').append('<option>' + year + '</option>');
-      };//sets dates in year dropdown
-    
-  var recordCollection = [1,2];
-  
-  var recordCollectionLength = recordCollection.length; 
-  
-  //localStorage.setItem("collection1", recordCollection); 
-  
-  localStorage.setItem("collection1", recordCollection);//redundant should it be record Props
-  
-  function displayCollection(){
-    $('#catalogue').append('<tr><td>' + localStorage.getItem("collection1") + '</td></tr>');
-  };//end display collection
-  
-  $('#listcount').append('<li id="listcount">' + recordCollectionLength + '</span>');
-  
-  
-  
 
   
-  function commitNewRecord(evt, recordCollection){
-    $('#addrecord').trigger("reset");
+  var searchInput = $('#searchInput').val();
+  function searchRecord(evt){
+  };//end search 
+  var searchButton = $('#search').on('click', searchRecord);
+  
+  
+  function clearLocal(evt){
+    localStorage.clear();
+  };//end clear
+  var clearButton = $('#clearlocal').on('click', clearLocal);
+  
+  
+  function showAddDisplay(evt){
+    $('#adddisplay').removeClass('hide');
+    $('#adddisplay').addClass('show');
+  };
+  var addButton = $('#addnew').on('click', showAddDisplay);
+  
+  
+  function hideAddDisplay(evt){
+    $('#adddisplay').removeClass('show');
+    $('#adddisplay').addClass('hide');
+  };  
+  var hideDisplayButton = $('#hidepanel').on('click', hideAddDisplay);
+  
+  
+  function syncServer(){
+    console.log("synced");
+  };//end sync  
+  var autoSync = ('#autosync');
     
+  var recordCollectionLength = localStorage.length;
+  
+  function displayCollection(){    
+    for(i = 0; i<=recordCollectionLength; i++){
+    key = localStorage.getItem(rcrecord(i));
+    $('#catalogue').append('<tr><td>' + key + '</td></tr>');
+    };
+  };
+  
+  if (recordCollectionLength > 0){
+    displayCollection;
+  };
+  
+  function commitNewRecord(evt){
+    //alert("active");
+    
+    var d = new Date();
+    var month = d.getMonth();
+    var date = d.getDate();
+    var year = d.getFullYear();
+
+    var dateString = year.toString().substr(2,2);
+
+    
+    var addDate = month+"-"+date+"-"+year;
     var addArtist = $('#artistInput').val();
     var addTitle = $('#titleInput').val();
     var addLabel = $('#labelInput').val();
-    var addDate = new Date();   
-    var addId = Math.random();
+    var addId = ((Math.random()) * 1000000000000000000);
     var addYear = $('#yearInput').val();
     var addSku = $('#skuInput').val();
     var addSelling = $('#saleInput').val();
@@ -86,57 +107,27 @@ function init(){
     recordProps.push(addId);
     recordProps.push(addDate);
     
-    recordCollection.push(recordProps);
+    localStorage.setItem("rcRecord" + recordProps[7], recordProps);
+    //console.log(recordProps[7]);
     
     if (autoSync.checked){
       syncServer;
     };
-    displayCollection();
-    //$('#catalogue').append('<tr><td>TEST</td></tr>');
-     
+    
+    $('#addrecord').trigger("reset");
+   
+    displayCollection;
+   
   };//end commitNewRecord();
+  var commitButton = $('#commitnew');
+  commitButton.off();
+  commitButton.on('click', commitNewRecord);
   
-  
-  function showAddDisplay(evt){
-    $('#adddisplay').removeClass('hide');
-    $('#adddisplay').addClass('show');
-  };
-  
-  function hideAddDisplay(evt){
-    $('#adddisplay').removeClass('show');
-    $('#adddisplay').addClass('hide');
-  };
   
   function logListResult(data){
       alert('Data received');
   };
-  
-  
-  function deleteRecord(evt){
-    localStorage.removeItem ( "rcrecord" );//remove an item
-  };//end delete
-  
-  function updateRecord(evt){//PUT//crudUPDATE
-//    evt.preventDefault();
-    var addArtist = $('#artist').val();
-    var addTitle = $('#title').val();
-    var addLabel = $('#label').val();
-    var addDate = new Date();
-  };//end update
-  
-  function searchRecord(evt){
-  };//end search 
-  
-  function syncServer(){
-    console.log("synced");
-  };//end sync
-  
-  function clearLocal(evt){
-    localStorage.clear();
-  };//end clear
-  
-  
-
- displayCollection();  
+   
+ 
 };//end init
 
